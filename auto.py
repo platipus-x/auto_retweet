81c7a9bf0fc3360f73e2b1ad28a2db9fc46865d0
x-platipus <rivanzaxa@gmail.com>
2021-04-09 16:13:24 +0700

    initial commit

---
+++ auto.py
@@ -0,0 +1,30 @@
+import tweepy as twitter
+import constant
+import time, datetime
+
+auth = twitter.OAuthHandler(constant.CONSUMER_KEY, constant.CONSUMER_SECRET)
+auth.set_access_token(constant.ACCESS_KEY, constant.ACCESS_SECRET)
+api = twitter.API(auth)
+
+
+def twitter_bot(username, delay):
+    while True:
+        print(f"\n{datetime.datetime.now()}\n")
+
+        for tweet in twitter.Cursor(api.search, q=username, rpp=10).items(5):
+            try:
+                tweet_id = dict(tweet._json)["id"]
+                tweet_text = dict(tweet._json)["text"]
+
+                print("id: " + str(tweet_id))
+                print("text: " + str(tweet_text))
+
+                api.retweet(tweet_id)
+
+            except twitter.TweepError as error:
+                print(error.reason)
+
+
+        time.sleep(3600)
+
+twitter_bot("from:_Weeekly", 10)
